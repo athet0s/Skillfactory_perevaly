344a58ae866cb90fe4727d6e624f696c5839d3b4
athet0s <lexel404@gmail.com>
2022-11-25 10:16:24 +0700

    moved connection creation from init to enter

--- classes.py
+++ classes.py
@@ -3,12 +3,17 @@ import psycopg2
 
 class DBManager:
     def __init__(self, name, host, login, password, port):
-        self._conn = psycopg2.connect(dbname=name, user=login,
-                                      password=password, host=host, port=port)
-
-        self._cursor = self._conn.cursor()
+        self._name = name
+        self._host = host
+        self._login = login
+        self._password = password
+        self._port = port
 
     def __enter__(self):
+        self._conn = psycopg2.connect(dbname=self._name, user=self._login,
+                                      password=self._password, host=self._host, port=self._port)
+
+        self._cursor = self._conn.cursor()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
