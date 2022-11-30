b428d4af6f930d7c74ca2ba247f5105893053bac
athet0s <lexel404@gmail.com>
2022-11-30 17:20:44 +0700

    Moved all sql to a separate file and made execute functions to take data as dictionaries

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 from datetime import datetime
+from sql import *
 
 load_dotenv()
 
@@ -43,43 +44,52 @@ class PerevalManager(DBManager):
 
     def _insert_or_update_user(self):
         user = self._data.user
-        self._cursor.execute('''INSERT INTO users (fam, name, otc, phone, email) 
-                                  VALUES (%s, %s, %s, %s, %s) 
-                                  ON CONFLICT(email) DO UPDATE SET
-                                  (fam, name, otc, phone) = (EXCLUDED.fam, EXCLUDED.name, EXCLUDED.otc, EXCLUDED.phone)
-                                  RETURNING id;
-                              ''',
-                             (user.fam, user.name, user.otc, user.phone, user.email))
+        self._cursor.execute(SQL_INSERT_OR_UPDATE_USERS,
+                             ({'fam': user.fam,
+                               'name': user.name,
+                               'otc': user.otc,
+                              'phone': user.phone,
+                               'email': user.email}))
         pk = self._cursor.fetchone()[0]
         return pk
 
     def _insert_coords(self):
         coords = self._data.coords
-        self._cursor.execute("INSERT INTO coords (latitude, longitude, height) VALUES (%s, %s, %s) RETURNING id",
-                             (float(coords.latitude), float(coords.longitude), int(coords.height)))
+        self._cursor.execute(SQL_INSERT_COORDS,
+                             ({'latitude': float(coords.latitude),
+                               'longitude': float(coords.longitude),
+                               'height': int(coords.height)}))
         pk = self._cursor.fetchone()[0]
         return pk
 
     def _insert_pereval(self, user_id, coord_id):
         data = self._data
         level = data.level
-        self._cursor.execute('''INSERT INTO pereval_added (beauty_title, title, other_titles, connect, add_time,
-                                    level_winter, level_summer, level_autumn, level_spring, user_id, coord_id, status,
-                                    date_added) 
-                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
-                                    RETURNING id;
-                             ''',
-                             (data.beauty_title, data.title, data.other_titles, data.connect, data.add_time,
-                              level.winter, level.summer, level.autumn, level.spring, user_id, coord_id,
-                              self._pereval_status, datetime.now()))
+        self._cursor.execute(SQL_INSERT_PEREVAL,
+                             ({'beauty_title': data.beauty_title,
+                               'title': data.title,
+                               'other_titles': data.other_titles,
+                               'connect': data.connect,
+                               'add_time': data.add_time,
+                               'level_winter': level.winter,
+                               'level_summer': level.summer,
+                               'level_autumn': level.autumn,
+                              'level_spring': level.spring,
+                               'user_id': user_id,
+                               'coord_id': coord_id,
+                               'status': self._pereval_status,
+                               'date_added': datetime.now()}))
 
         pk = self._cursor.fetchone()[0]
         return pk
 
     def _insert_images(self, pereval_id):
         for i in self._data.images:
-            self._cursor.execute("INSERT INTO pereval_images (title, img, date_added) VALUES (%s, %s, %s) RETURNING id",
-                                 (i.title, i.data, datetime.now()))
+            self._cursor.execute(SQL_INSERT_PEREVAL_IMAGES,
+                                 ({'title': i.title,
+                                   'img': i.data,
+                                   'date_added': datetime.now()}))
             image_id = self._cursor.fetchone()[0]
-            self._cursor.execute("INSERT INTO pereval_added_pereval_images (pereval_id, image_id) VALUES (%s, %s)",
-                                 (pereval_id, image_id))
+            self._cursor.execute(SQL_INSERT_PEREVAL_ADDED_PEREVAL_IMAGES,
+                                 ({'pereval_id': pereval_id,
+                                   'image_id': image_id}))
---
+++ sql.py
@@ -0,0 +1,29 @@
+SQL_INSERT_OR_UPDATE_USERS = '''INSERT INTO users (fam, name, otc, phone, email) 
+                                  VALUES (%(fam)s, %(name)s, %(otc)s, %(phone)s, %(email)s) 
+                                  ON CONFLICT(email) DO UPDATE SET
+                                  (fam, name, otc, phone) = (EXCLUDED.fam, EXCLUDED.name, EXCLUDED.otc, EXCLUDED.phone)
+                                  RETURNING id;
+                              '''
+
+SQL_INSERT_PEREVAL = '''INSERT INTO pereval_added (beauty_title, title, other_titles, connect, add_time,
+                                level_winter, level_summer, level_autumn, level_spring, user_id, coord_id, status,
+                                date_added) 
+                            VALUES (%(beauty_title)s, %(title)s, %(other_titles)s, %(connect)s, %(add_time)s, 
+                                %(level_winter)s, %(level_summer)s, %(level_autumn)s, %(level_spring)s, %(user_id)s, 
+                                %(coord_id)s, %(status)s, %(date_added)s) 
+                            RETURNING id;
+                     '''
+
+SQL_INSERT_COORDS = '''INSERT INTO coords (latitude, longitude, height) 
+                            VALUES (%(latitude)s, %(longitude)s, %(height)s) 
+                            RETURNING id
+                    '''
+
+SQL_INSERT_PEREVAL_IMAGES = '''INSERT INTO pereval_images (title, img, date_added) 
+                                    VALUES (%(title)s, %(img)s, %(date_added)s) 
+                                    RETURNING id
+                            '''
+
+SQL_INSERT_PEREVAL_ADDED_PEREVAL_IMAGES = '''INSERT INTO pereval_added_pereval_images (pereval_id, image_id)
+                                                VALUES (%(pereval_id)s, %(image_id)s)
+                                          '''
